c2aedac800939badb4e650f0a340cd7b65a2ccbf
JonatanPache <yonh.guitar777@gmail.com>
2024-06-16 20:36:36 -0400

    asd

--- deff_site_web/wsgi.py
+++ deff_site_web/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deff_site_web.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deff_site_web.conf.dev')
 
 application = get_wsgi_application()
